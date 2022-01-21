d73e0deb8b9fd32437f46c77c40aa1d57a9942bf
Tudor <53438489+TudorPetrariu@users.noreply.github.com>
2022-01-21 18:49:07 +0100

    Rework async await (#2)
    * Install axios and json-server
    
    * Setup axios
    
    * Setup local DB
    
    * Fetch tableData from store to replicate a better async await
    
    * Add mapActions and mapGetters for store managment
    
    * Update readme

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { TableData } from "@/types/types";
+import api from "../../plugins/axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: () => ({
+    tableResources: Array<TableData>()
+  }),
+  getters: {
+    tableResources(state) {
+      return state.tableResources
+    }
+  },
+  mutations: {
+    setTableData(state, data) {
+      state.tableResources = data
+    }
+  },
+  actions: {
+    async getTableData({ commit }) {
+      try {
+        const { data }: { data: TableData[] } = await api.get(`/resources`)
+        commit('setTableData', data)
+      } catch (error) {
+        console.log('Cannot getTableData', error);
+      }
+    },
+  },
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>This is a table with some important data</h1>
-    <b-table :data="tableData" :columns="columns">
+    <b-table :data="tableResources" :columns="columns">
       <template #footer>
         <th key-attribute="footer-label">
           <span>TOTALS</span>
@@ -17,6 +17,7 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { mapActions, mapGetters } from "vuex";
 import { TableData } from "@/types/types";
 import { totalAmount } from "../../utils/helperFunctions";
 import transferRowFooter from "@/components/transferRowFooter.vue";
@@ -24,8 +25,22 @@ import transferRowFooter from "@/components/transferRowFooter.vue";
 @Component({
   name: "Home",
   components: { transferRowFooter },
+  computed: {
+    ...mapGetters(["tableResources"]),
+  },
+  methods: {
+    ...mapActions(["getTableData", "addSecurityClass"]),
+  },
 })
 export default class Home extends Vue {
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  async created() {
+    await this.getTableData();
+  }
+  getTableData!: () => any;
+  tableResources!: () => any;
+  
   tableData: TableData[] = [];
   columns = [
     {
@@ -51,79 +66,79 @@ export default class Home extends Vue {
   ];
   loading = false;
   get totalAuthorizedAmount(): number {
-    return totalAmount(this.tableData, "authorizedAmount");
+    return totalAmount(this.tableResources, "authorizedAmount");
   }
   get totalIssuedAmount(): number {
-    return totalAmount(this.tableData, "issuedAmount");
+    return totalAmount(this.tableResources, "issuedAmount");
   }
   get totalAuthorizedCapital(): number {
-    return totalAmount(this.tableData, "authorizedCapital");
+    return totalAmount(this.tableResources, "authorizedCapital");
   }
   get totalIssuedCapital(): number {
-    return totalAmount(this.tableData, "issuedCapital");
+    return totalAmount(this.tableResources, "issuedCapital");
   }
 
   // mounted works fine if your ide complains about it
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  mounted() {
-    this.getData()
-      .then((data: TableData[]) => {
-        this.loading = true;
-        return data.map((dataItem: TableData) => {
-          return {
-            ...dataItem,
-            randomNumber: Math.random(),
-          };
-        });
-      })
-      .then((data: TableData[]) => {
-        this.tableData = data;
-        this.loading = false;
-      })
-      .catch((error) => {
-        console.log(error, "This is not good");
-      });
-  }
+  // mounted() {
+  //   this.getData()
+  //     .then((data: TableData[]) => {
+  //       this.loading = true;
+  //       return data.map((dataItem: TableData) => {
+  //         return {
+  //           ...dataItem,
+  //           randomNumber: Math.random(),
+  //         };
+  //       });
+  //     })
+  //     .then((data: TableData[]) => {
+  //       this.tableData = data;
+  //       this.loading = false;
+  //     })
+  //     .catch((error) => {
+  //       console.log(error, "This is not good");
+  //     });
+  // }
 
-  async getData(): Promise<TableData[]> {
-    return [
-      {
-        id: "42f2462d-49d0-4e91-8fe1-de2e656b0f06",
-        name: "Series A",
-        nominalValue: 5,
-        authorizedAmount: 1500,
-        issuedAmount: 500,
-        authorizedCapital: 7550,
-        issuedCapital: 2500,
-      },
-      {
-        id: "42f2462d-49d0-4e91-8fe1-de2e656b0f06",
-        name: "Series B",
-        nominalValue: 10,
-        authorizedAmount: 15000,
-        issuedAmount: 5000,
-        authorizedCapital: 150000,
-        issuedCapital: 50000,
-      },
-      {
-        id: "fd78c11b-e3d2-455a-99b0-49907a75c463",
-        name: "Series C",
-        nominalValue: 1,
-        authorizedAmount: 96876,
-        issuedAmount: 61760,
-        authorizedCapital: 96876,
-        issuedCapital: 61760,
-      },
-      {
-        id: "d8654cb0-8986-4fbc-b969-025e514cb934",
-        name: "Series D",
-        nominalValue: 1,
-        authorizedAmount: 10110,
-        issuedAmount: 1100,
-        authorizedCapital: 10110,
-        issuedCapital: 1100,
-      },
-    ];
-  }
+  // async getData(): Promise<TableData[]> {
+  //   return [
+  //     {
+  //       id: "42f2462d-49d0-4e91-8fe1-de2e656b0f06",
+  //       name: "Series A",
+  //       nominalValue: 5,
+  //       authorizedAmount: 1500,
+  //       issuedAmount: 500,
+  //       authorizedCapital: 7550,
+  //       issuedCapital: 2500,
+  //     },
+  //     {
+  //       id: "42f2462d-49d0-4e91-8fe1-de2e656b0f06",
+  //       name: "Series B",
+  //       nominalValue: 10,
+  //       authorizedAmount: 15000,
+  //       issuedAmount: 5000,
+  //       authorizedCapital: 150000,
+  //       issuedCapital: 50000,
+  //     },
+  //     {
+  //       id: "fd78c11b-e3d2-455a-99b0-49907a75c463",
+  //       name: "Series C",
+  //       nominalValue: 1,
+  //       authorizedAmount: 96876,
+  //       issuedAmount: 61760,
+  //       authorizedCapital: 96876,
+  //       issuedCapital: 61760,
+  //     },
+  //     {
+  //       id: "d8654cb0-8986-4fbc-b969-025e514cb934",
+  //       name: "Series D",
+  //       nominalValue: 1,
+  //       authorizedAmount: 10110,
+  //       issuedAmount: 1100,
+  //       authorizedCapital: 10110,
+  //       issuedCapital: 1100,
+  //     },
+  //   ];
+  // }
 }
 </script>
